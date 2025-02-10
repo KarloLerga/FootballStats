1dc4e29fa3f7dfb261343de66425cf4c7c7c0097
karlo <karlolerga16@gmail.com>
2025-02-10 22:13:57 +0100

    Ikone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,10 +40,12 @@ android {
 }
 
 dependencies {
+
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.activity:activity-compose:1.7.2")
-
-    // Retrofit za API pozive
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.compose.material3:material3:1.1.1") // Material3
@@ -51,15 +53,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:1.4.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.5.3")
-
-    // Coil za učitavanje slika
     implementation("io.coil-kt:coil-compose:2.2.2")
-
-    // Retrofit za API pozive
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("com.google.accompanist:accompanist-swiperefresh:0.31.1-alpha")
 
